20d8e545897f4be4253502cf8cd241c2a88694bb
Marc Brinkmann <git@marcbrinkmann.de>
2016-08-15 21:14:00 +0200

    Added central message widget.

--- src/window.rs
+++ src/window.rs
@@ -1,5 +1,7 @@
 use rustty::Cell;
-use super::{DrawingContext, Widget};
+use std::ascii::AsciiExt;
+use std::cmp::min;
+use super::{DEFAULT_STYLE, DrawingContext, Style, Widget};
 
 
 pub struct FramedWindow {
@@ -36,5 +38,71 @@ impl Widget for FramedWindow {
         ctx.line((right, top), (right, bottom), self.frame_cell);
         ctx.line((right, bottom), (left, bottom), self.frame_cell);
         ctx.line((left, bottom), (left, top), self.frame_cell);
+    }
+}
+
+
+pub struct MessageFill<'a> {
+    bg_cell: Cell,
+    msg: &'a str,
+    padding: usize,
+    style: Style,
+}
+
+impl<'a> MessageFill<'a> {
+    pub fn new() -> MessageFill<'a> {
+        MessageFill {
+            bg_cell: Cell::new(' ', DEFAULT_STYLE.0, DEFAULT_STYLE.1, DEFAULT_STYLE.2),
+            msg: "",
+            padding: 2,
+            style: DEFAULT_STYLE,
+        }
+    }
+
+    pub fn bg_cell(mut self, bg_cell: Cell) -> MessageFill<'a> {
+        self.bg_cell = bg_cell;
+        self
+    }
+
+    pub fn msg(mut self, msg: &'a str) -> MessageFill<'a> {
+        self.msg = msg;
+        self
+    }
+
+    pub fn padding(mut self, padding: usize) -> MessageFill<'a> {
+        self.padding = padding;
+        self
+    }
+
+    pub fn style(mut self, style: Style) -> MessageFill<'a> {
+        self.style = style;
+        self
+    }
+}
+
+impl<'a> Widget for MessageFill<'a> {
+    fn draw_on(&self, ctx: &mut DrawingContext) {
+        assert!(self.msg.is_ascii());
+        // FIXME: proper word-wrapping would be good here
+        // get line length
+
+        let (cols, rows) = ctx.size();
+
+        if cols <= self.padding * 2 || rows < 1 {
+            return;
+        }
+
+        let tx_row_len = cols - 2 * self.padding;
+
+        let tx_rows = self.msg.len() % tx_row_len;
+
+        for n in 0..tx_rows {
+            let start = min(n * tx_row_len, self.msg.len());
+            let end = min(self.msg.len(), start + tx_row_len);
+            ctx.text((tx_row_len / 2 + self.padding, rows.saturating_sub(tx_rows) / 2 + n),
+                     &self.msg[start..end],
+                     self.style)
+        }
+
     }
 }
