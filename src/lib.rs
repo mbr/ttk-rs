6a809620f10e384f377c7c8063ae1cf09e193f0b
Marc Brinkmann <git@marcbrinkmann.de>
2016-08-16 12:29:38 +0200

    Added MainLoop struct.

--- src/lib.rs
+++ src/lib.rs
@@ -27,65 +27,90 @@ fn draw_view<T: Application>(term: sync::Arc<sync::Mutex<rustty::Terminal>>, app
     t.swap_buffers().unwrap();
 }
 
-pub fn run_app<T: Application>(app: T) {
-    run_app_with_setup(app, |_| {})
+pub struct MainLoop {
+    min_delay: Option<time::Duration>,
 }
 
-
-pub fn run_app_with_setup<T: Application, F>(mut app: T, setup: F)
-    where F: FnOnce(sync::mpsc::Sender<<T as Application>::Action>) -> ()
-{
-    // unfortunately, the rustty API is pretty bad here; we cannot draw on the
-    // terminal while receiving events from it. for this reason, we need to
-    // lock and poll ...
-    let term = sync::Arc::new(sync::Mutex::new(rustty::Terminal::new().unwrap()));
-
-    let (action_send, action_recv) = sync::mpsc::channel();
-    let running = sync::Arc::new(sync::atomic::AtomicBool::new(true));
-
-    // run setup function
-    setup(action_send.clone());
-
-    // start background thread that turns events into actions
-    let bg_term = term.clone();
-    let bg_running = running.clone();
-    let bg_thread = thread::spawn(move || {
-        loop {
-            // check if app is alive
-            if !bg_running.load(sync::atomic::Ordering::Relaxed) {
-                break;
-            }
-
-            let mut t = bg_term.lock().unwrap();
-            match t.get_event(time::Duration::from_millis(10)).unwrap() {
-                None => continue,
-                Some(ev) => {
-                    let act: <T as Application>::Action = ev.into();
-                    action_send.send(act).unwrap();
-                }
-            }
-        }
-    });
-
-    // draw once initially
-    draw_view(term.clone(), &app);
-
-    for action in action_recv.iter() {
-        // update state according to action
-        let (napp, ntask) = app.handle_action(action);
-        app = napp;
-
-        if let Some(task) = ntask {
-            if app.exec_task(task) {
-                break;
-            }
-        }
-
-        // redraw
-        draw_view(term.clone(), &app);
+impl MainLoop {
+    pub fn new() -> MainLoop {
+        MainLoop { min_delay: None }
     }
 
-    // shutdown background thread for clean terminal restoration
-    running.store(false, sync::atomic::Ordering::Relaxed);
-    bg_thread.join().unwrap();
+    pub fn min_delay(&mut self, delay: time::Duration) -> &mut MainLoop {
+        self.min_delay = Some(delay);
+        self
+    }
+
+    pub fn unlimited(&mut self) -> &mut MainLoop {
+        self.min_delay = None;
+        self
+    }
+
+    pub fn max_fps(&mut self, fps: u64) -> &mut MainLoop {
+        self.min_delay(time::Duration::from_millis(1000 / fps));
+        self
+    }
+
+    pub fn run<T: Application>(&mut self, mut app: T) {
+        self.run_with_setup(app, |_| {});
+    }
+
+    pub fn run_with_setup<T: Application, F>(&mut self, mut app: T, setup: F)
+        where F: FnOnce(sync::mpsc::Sender<<T as Application>::Action>) -> ()
+    {
+        // unfortunately, the rustty API is pretty bad here; we cannot draw on the
+        // terminal while receiving events from it. for this reason, we need to
+        // lock and poll ...
+        let term = sync::Arc::new(sync::Mutex::new(rustty::Terminal::new().unwrap()));
+
+        let (action_send, action_recv) = sync::mpsc::channel();
+        let running = sync::Arc::new(sync::atomic::AtomicBool::new(true));
+
+        // run setup function
+        setup(action_send.clone());
+
+        // start background thread that turns events into actions
+        let bg_term = term.clone();
+        let bg_running = running.clone();
+        let bg_thread = thread::spawn(move || {
+            loop {
+                // check if app is alive
+                if !bg_running.load(sync::atomic::Ordering::Relaxed) {
+                    break;
+                }
+
+                let mut t = bg_term.lock().unwrap();
+                match t.get_event(time::Duration::from_millis(10)).unwrap() {
+                    None => continue,
+                    Some(ev) => {
+                        let act: <T as Application>::Action = ev.into();
+                        action_send.send(act).unwrap();
+                    }
+                }
+            }
+        });
+
+        // draw once initially
+        draw_view(term.clone(), &app);
+
+        for action in action_recv.iter() {
+            // update state according to action
+            let (napp, ntask) = app.handle_action(action);
+            app = napp;
+
+            if let Some(task) = ntask {
+                if app.exec_task(task) {
+                    break;
+                }
+            }
+
+            // redraw
+            // FIXME: limit framerate
+            draw_view(term.clone(), &app);
+        }
+
+        // shutdown background thread for clean terminal restoration
+        running.store(false, sync::atomic::Ordering::Relaxed);
+        bg_thread.join().unwrap();
+    }
 }
